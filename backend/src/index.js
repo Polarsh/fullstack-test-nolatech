389bb7ec548d3f44c03a12ec98a8554e767364a1
Polar <dybsm98@outlook.es>
2024-10-22 16:14:57 -0500

    se añade cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 
@@ -9,11 +10,30 @@ import evaluationRoutes from "./routes/evaluations.js";
 import feedbackRoutes from "./routes/feedback.js";
 import reportRoutes from "./routes/reports.js";
 
+const app = express();
+
 // Config
 dotenv.config();
 connectDB();
 
-const app = express();
+// Configurar CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Asegúrate de que el origen sea el correcto
+    methods: "GET,POST,PUT,DELETE,OPTIONS", // Agregar OPTIONS aquí también
+    allowedHeaders: "Content-Type, Authorization", // Asegúrate de permitir estos encabezados
+    credentials: true, // Permitir el uso de cookies, tokens, etc.
+  })
+);
+
+// Manejo de preflight requests
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
 
 // Middlewares
 app.use(express.json());
