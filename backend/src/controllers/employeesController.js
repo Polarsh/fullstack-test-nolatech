9d49353d0b7fa99a81c394f4eef47fd200f55cc8
Polar <dybsm98@outlook.es>
2024-10-23 18:09:35 -0500

    se añade obtener employee por id

--- backend/src/controllers/employeesController.js
+++ backend/src/controllers/employeesController.js
@@ -12,7 +12,39 @@ export const getEmployees = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       error: {
-        message: "Error al obtener los usuarios",
+        message: "Error al obtener los empleados",
+        details: error.message,
+      },
+      data: null,
+    });
+  }
+};
+
+// Obtener una empleado or ID
+export const getEmployeeById = async (req, res) => {
+  const { id: employeeId } = req.params;
+
+  try {
+    const employee = await Employee.findById(employeeId);
+
+    if (!employee) {
+      return res.status(404).json({
+        error: { message: "Empleado no encontrada." },
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      error: null,
+      data: {
+        message: "Empleado encontrado.",
+        data: employee,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        message: "Error al obtener el empleado.",
         details: error.message,
       },
       data: null,
--- backend/src/routes/employees.js
+++ backend/src/routes/employees.js
@@ -1,12 +1,26 @@
 import { Router } from "express";
 
-import authMiddleware from "../middleware/auth.js";
+import authenticateAndAuthorizeRoles from "../middleware/auth.js";
 
-import { getEmployees } from "../controllers/employeesController.js";
+import {
+  getEmployeeById,
+  getEmployees,
+} from "../controllers/employeesController.js";
 
 const router = Router();
 
 // Obtener lista de empleados
-router.get("", getEmployees);
+router.get(
+  "",
+  authenticateAndAuthorizeRoles("Admin", "Manager", "Employee"),
+  getEmployees
+);
+
+// Obtener evaluaciones de un empleado
+router.get(
+  "/:id",
+  authenticateAndAuthorizeRoles("Admin", "Manager", "Employee"),
+  getEmployeeById
+);
 
 export default router;
